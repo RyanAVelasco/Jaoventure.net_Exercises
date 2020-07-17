8ceaa9e3c6f7a07d3e8a308daedf91a879999ee8
RyanAVelasco <45979367+RyanAVelasco@users.noreply.github.com>
2020-07-17 14:00:39 -0500

    more questions

---
+++ Chapter 4.py
@@ -0,0 +1,54 @@
+import math
+
+print("""\n\n4.1: Exercises with the math module
+Use the python documentation about the math module (https://docs.python.org/3/library/math.html)
+to solve the following exercises:
+1. Find the greatest common divisor of the following pairs of numbers: (15, 21), (152,200), (1988, 9765).
+2. Compute the base-2 logarithm of the following numbers: 0, 1, 2, 6, 9, 15.
+3. Use the "input" function to ask the user for a number and show the result of the sine, cosine and tangent
+of the number. 
+Make sure that you convert the user input
+from string to a number (use the int() or the float() function).""")
+
+print("""\n=== Answer 1 ===""")
+
+print("""\n=== Answer 2 ===""")
+
+print("""\n=== Answer 3 ===""")
+
+print("""\n\n4.2: Exercises with functions
+1. Implement the "add2" function that receives two numbers as arguments and returns the sum of the numbers. 
+Then implement the "add3" function that receives and sums 3 parameters.
+2. Implement a function that returns the greatest of two numbers given as parameters.
+Use the "if" statement to compare both numbers: https://docs.python.org/3/tutorial/controlflow.html#if-statements.
+3. Implement a function named "is_divisable" that receives two parameters (named"a" and "b") 
+and returns true if "a" can be divided by "b" or false otherwise. 
+A number is divisable by another when the remainder of the division is zero. 
+Use the modulo operator ("%").
+4. Create a function named "average" that computes the average value of a list passed as parameter to the function. 
+Use the "sum" and "len" functions.""")
+
+print("""\n=== Answer 1 ===""")
+
+print("""\n=== Answer 2 ===""")
+
+print("""\n=== Answer 3 ===""")
+
+print("""\n=== Answer 4 ===""")
+
+print("""\n\n4.4: Exercises with recursive functions
+1. Implement the factorial function and test it with several different values. 
+Cross-check with a calculator.
+2. Implement a recursive function to compute the sum of then first integer numbers(wheren is a function parameter). 
+Start by thinking about the base case (the sum of the first 0 integers is?) and then think about the recursive case.
+3. The Fibonnaci sequence is a sequence of numbers in which each number of the sequence matches 
+the sum of the previous two terms. 
+Given the following recursive definition implement fib(n).
+#  See PDF for fib(n) image
+Check your results for the first numbers of the sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21,34, 55, 89, ...""")
+
+print("""\n=== Answer 1 ===""")
+
+print("""\n=== Answer 2 ===""")
+
+print("""\n=== Answer 3 ===""")
